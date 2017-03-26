d52d19077a2d578df131c6ccb569973dd0d7ebca
Roman Oderov <roman.oderov@dell.com>
2017-03-26 10:46:09 +0300

    W5. PointSET tests passed. 51/100

--- src/kdtree/PointSET.java
+++ src/kdtree/PointSET.java
@@ -2,6 +2,11 @@ package kdtree;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by oderor on 3/25/2017.
@@ -9,44 +14,81 @@ import edu.princeton.cs.algs4.RectHV;
 
 public class PointSET {
 
+    private final SET<Point2D> pointSet;
+
     // construct an empty set of points
     public PointSET() {
-
+        pointSet = new SET<>();
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return true;
+        return pointSet.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return pointSet.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        // if (pointSet.contains(p)){
+        //     return;
+        // }
+        pointSet.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return true;
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        return pointSet.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
-
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        for (Point2D p : pointSet) {
+            p.draw();
+        }
     }
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        if (rect == null) {
+            throw new NullPointerException();
+        }
+        List<Point2D> pointsInRect = new ArrayList<>();
+        for (Point2D p : pointSet) {
+            if (rect.contains(p)) {
+                pointsInRect.add(p);
+            }
+        }
+        return pointsInRect;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return null;
+        if (p == null) {
+            throw new NullPointerException();
+        }
+        Point2D nearestPoint = null;
+        double nearestDist = Double.MAX_VALUE;
+        double thatDist;
+        for (Point2D that : pointSet) {
+            thatDist = p.distanceSquaredTo(that);
+            if (thatDist < nearestDist) {
+                nearestDist = thatDist;
+                nearestPoint = that;
+            }
+        }
+        return nearestPoint;
     }
 
     // unit testing of the methods (optional)
